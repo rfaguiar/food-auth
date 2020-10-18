4f03f18ca5e17d76aa6a99ff2477188bbd76478d
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-10-18 15:45:08 -0300

    Assinando o JWT com RSA SHA-256 (chave assimétrica)

--- src/main/java/com/food/auth/config/AuthorizationServerConfig.java
+++ src/main/java/com/food/auth/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.food.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import java.util.List;
@@ -90,7 +92,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("asgfasdgferqwtfdsgshgwstgqw4523refdsag234tg");
+        //chave simatrica
+//        jwtAccessTokenConverter.setSigningKey("asgfasdgferqwtfdsgshgwstgqw4523refdsag234tg");
+        //chave assimetrica
+        var jksResource = new ClassPathResource("keystores/food-api.jks");
+        var keyStorePass = "123456789";
+        var keyPairAlias = "food-api";
+        var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+        var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+
+        jwtAccessTokenConverter.setKeyPair(keyPair);
+
         return jwtAccessTokenConverter;
     }
 
